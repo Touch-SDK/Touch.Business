478a9607ce920454bee4ee589394fb2e7ee505dd
Artem Abashev <artem@abashev.com>
2014-06-24 21:29:17 +0400

    Membership logic.

---
+++ Touch.Business.Authentication/Domain/Membership.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Touch.Persistence;
+
+namespace Touch.Domain
+{
+    /// <summary>
+    /// Access token.
+    /// </summary>
+    [DataContract(Name = "Membership")]
+    public class Membership : Document
+    {
+        [DataMember(Name = "token", Order = 0)]
+        public override string HashKey { get; set; }
+
+        [DataMember(Name = "username", Order = 0)]
+        public string UserName { get; set; }
+
+        [DataMember(Name = "password", Order = 1)]
+        public string Password { get; set; }
+
+        [DataMember(Name = "salt", Order = 2)]
+        public string Salt { get; set; }
+
+        [DataMember(Name = "email", Order = 3)]
+        public string Email { get; set; }
+
+        [DataMember(Name = "is_approved", Order = 4)]
+        public bool IsApproved { get; set; }
+
+        [DataMember(Name = "is_lockeout", Order = 5)]
+        public bool IsLockedOut { get; set; }
+
+        [DataMember(Name = "create_date", Order = 6)]
+        public DateTime CreateDate { get; set; }
+
+        [DataMember(Name = "last_login_date", Order = 7)]
+        public DateTime? LastLoginDate { get; set; }
+
+        [DataMember(Name = "last_password_changed_date", Order = 8)]
+        public DateTime? LastPasswordChangedDate { get; set; }
+
+        [DataMember(Name = "last_locked_out_date", Order = 9)]
+        public DateTime? LastLockedOutDate { get; set; }
+
+        [DataMember(Name = "last_activity_date", Order = 10)]
+        public DateTime? LastActivityDate { get; set; }
+
+        [DataMember(Name = "failed_password_attempt_count", Order = 11)]
+        public int FailedPasswordAttemptCount { get; set; }
+
+        [DataMember(Name = "failed_password_attempt_window_start", Order = 12)]
+        public DateTime? FailedPasswordAttemptWindowStart { get; set; }
+
+        [DataMember(Name = "failed_password_answer_attempt_count", Order = 13)]
+        public int FailedPasswordAnswerAttemptCount { get; set; }
+    }
+}
---
+++ Touch.Business.Authentication/Logic/MembershipLogic.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Touch.Domain;
+
+namespace Touch.Logic
+{
+    /// <summary>
+    /// Membership logic.
+    /// </summary>
+    public abstract class MembershipLogic
+    {
+        /// <summary>
+        /// Get user membership by token.
+        /// </summary>
+        /// <returns>Membership instance of <c>null</c> if user not found.</returns>
+        public abstract Membership GetUserByToken(string token);
+
+        /// <summary>
+        /// Get user membership by user name.
+        /// </summary>
+        /// <returns>Membership instance of <c>null</c> if user not found.</returns>
+        public abstract Membership GetUserByName(string username);
+
+        /// <summary>
+        /// Get user name by e-mail.
+        /// </summary>
+        /// <returns>User name of <c>null</c> if user not found.</returns>
+        public abstract string GetUserNameByEMail(string email);
+
+        /// <summary>
+        /// Persist new user membership.
+        /// </summary>
+        public abstract void SaveUser(Membership membership);
+
+        /// <summary>
+        /// Persist existing user membership.
+        /// </summary>
+        public abstract void UpdateUser(Membership membership);
+
+        /// <summary>
+        /// Delete existing user membership.
+        /// </summary>
+        public abstract void DeleteUser(Membership membership);
+
+        /// <summary>
+        /// Get all existing users within a range.
+        /// </summary>
+        public abstract IEnumerable<Membership> GetAllUsers(int pageIndex, int pageSize);
+
+        /// <summary>
+        /// Count the online users.
+        /// </summary>
+        public abstract int CountOnlineUsers(DateTime compareTime);
+    }
+}
