579ff18b82674ead22fc663645040f078feaaa41
Artem Abashev <artem@abashev.com>
2014-12-16 14:59:47 +0300

    Removed OAuth1 logic.

---
+++ Touch.Business.Authentication/Logic/IOAuth2Logic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Touch.Domain;
+
+namespace Touch.Logic
+{
+    public interface IOAuth2Logic
+    {
+        OAuth2Client GetClient(string clientId);
+
+        void StoreNonce(OAuth2Nonce nonce);
+
+        OAuth2Key GetKey(string bucket, string handle);
+
+        IEnumerable<OAuth2Key> GetKeys(string bucket);
+
+        void StoreKey(OAuth2Key key);
+
+        void RemoveKey(string bucket, string handle);
+    }
+}
